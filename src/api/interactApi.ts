19fa7ec617d6a110e1414b2122d8980bab43e3ac
HuyHoangFSDev <nguyenthehuyhoang2002@gmail.com>
2025-06-03 19:27:53 +0700

    Feat: Fix api report 404

--- src/api/interactApi.ts
+++ src/api/interactApi.ts
@@ -12,7 +12,7 @@ export const interactApi = {
   createReport: async (
     request: CreateReportRequest
   ): Promise<ResponseBase<null>> => {
-    const url = `/interact/report/create`;
+    const url = `/report/createt`;
     const response = await interactRepository.post(url, request);
     if (!response) {
       throw new Error("Failed to create report");
